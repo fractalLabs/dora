032c55902eeebc9145bd129eb2ae8285dacbddfb
Andres Gomez Urquiza <andres@fractal.ai>
2016-06-16 00:18:53 -0500

    quite endpoint de dora-view

--- src/dora/server.clj
+++ src/dora/server.clj
@@ -54,10 +54,6 @@
             (repl-route params)))
    ;(ANY "/validate" [& params]
    ;     (json (validate (:url params))))
-   (ANY "/fusion" [& params]
-        (println "Params: " params)
-        (println "Dora-view: " (dora-view params))
-        (json (dora-view params)))
    (ANY "/resource" [& params]
         (slurp (data-directory (:id (db-findf :resources params)))))
    (GET "/db" [& params]
