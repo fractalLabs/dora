e30cca2c76153e12ad84aaf80744d2ece72b8cd3
Andres Gomez Urquiza <andres@fractal.ai>
2016-07-11 19:20:05 -0500

    csv importer

---
+++ src/dora/importer.clj
@@ -0,0 +1,25 @@
+(ns dora.importer
+  (:require [dora.util :refer :all]
+            [mongerr.core :refer [db-insert]]
+            [nillib.formats :refer [csv]]
+            [nillib.worm :refer :all]))
+
+
+
+
+(defn extract-csvs [folder]
+  (let [files (ls-fr folder)]
+    (map csv files)))
+
+(defn import-csvs [collection folder]
+  (doall (pmap #(db-insert collection %)
+              (extract-csvs folder))))
+
+(defn digest [collection folder digested errors]
+  (doall (pmap #(try (db-insert collection (digitalize (csv %)))
+                     (mv % digested)
+                     (catch Exception e (db-insert :digest-error {:collection collection :exception e :file %})
+                            (mv % errors)))
+               (ls-fr folder))))
+
+                                        ;(digest :test-profeco "/Users/nex/Desktop/testin/origin" "/Users/nex/Desktop/testin/destiny" "/Users/nex/Desktop/testin/errors")
