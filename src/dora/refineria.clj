15e8c2e249f58088651b06d0026bd1e2565bb6f7
Andres Gomez Urquiza <andres@fractal.ai>
2016-09-08 14:26:39 -0500

    refineria VID

--- src/dora/refineria.clj
+++ src/dora/refineria.clj
@@ -15,6 +15,13 @@
 ;; Por ahora los recursos vip. despues sera (db :resources)
 (def mirrored-resources (map resource ["http://web.coneval.gob.mx/Informes/Pobreza/Datos_abiertos/Rezago-social-2000-2005-2010_mun_DA.csv" "http://www.cofepris.gob.mx/Transparencia/Documents/datos_abiertos/Agua_Calidad.csv" "http://201.116.60.46/DatosAbiertos/Datos_de_calidad_del_agua_de_5000_sitios_de_monitoreo.zip" "http://www.siap.gob.mx/datosAbiertos/Estadist_Produc_Agricola/Agt_cierre_2013_2014.csv" "http://www.datos.economia.gob.mx/RegulacionMinera/ConcesionesMineras.csv" "http://dsiappsdev.semarnat.gob.mx/datos/aire/datos%20RETC.csv" "http://www.sectur.gob.mx/DATOS/0116/19-Inventario-Turistico-por-entidad-federativa.csv" "http://www.sectur.gob.mx/DATOS/0116/24-Localidades-que-cuentan-con-el-nombramiento-de-Pueblo-Magico.csv" "http://www.datatur.beta.sectur.gob.mx/Documentos%20compartidos/6_1_td.csv" "http://catalogo.datos.gob.mx/dataset/54ae9a90-418c-4088-88d0-edea59814826/resource/ffc1323a-bf46-4d9d-86a8-237315c2036e/download/matriculaporinstitucionyentidadfederativa.csv" "http://data.sct.gob.mx:8082/datos/datos/abiertos/11601MexicoConectado.xlsx" "http://www.correosdemexico.gob.mx/datosabiertos/cp/cpdescarga.txt" "http://www.correosdemexico.gob.mx/datosabiertos/poligonos/mapapoligonos.zip"]))
 
+(def mirrored-datasets (mapcat #(:resources (dataset %))
+                               ["directorio-estadistico-nacional-de-unidades-economicas-denue-por-actividad-economica"
+                                "indicadores-urbanos"
+                                "censo-de-escuelas-maestros-y-alumnos-de-educacion-basica-y-especial"
+                                "proyecciones-de-la-poblacion-de-mexico"
+                                "regionalizacion-funcional-de-mexico"]))
+
 (def ^:dynamic refineria-dir "/Users/nex/mirrors/")
 (def ^:dynamic gh-org "mxabierto-mirror/")
 
@@ -118,6 +125,7 @@
 (defn update-buda [resource-name file]
   (let [batches (partition-all 100 (csv file))]
     (binding [*db* (:db (mg/connect-via-uri "mongodb://localhost:27027/buda"))]
+      (db-delete resource-name)
       (emap #(db-insert resource-name %) batches))))
 
 (defn apify
@@ -127,13 +135,15 @@
          files)))
 
 (defn mirror
-  ([] (doall (map mirror mirrored-resources)))
+  ([] (doall (map mirror mirrored-resources)));later pmap
   ([resource]
    (let [dir (mirror-dir resource)]
      (try
        (println "mirroring: " (:name resource))
        (repo resource)
-       (copy-resource resource)
+       (try
+         (copy-resource resource)
+         (catch Exception e (println "unable to download " (:name resource))))
        (adda dir)
        (commit (str "Generado por la refinería en: " (t/now)) dir)
        (push dir "origin" "master")
@@ -143,5 +153,5 @@
        (adda dir)
        (commit (str "Generado por la refinería en: " (t/now)) dir)
        (push-force dir "origin" "refineria")
-       (apify dir resource)
+       (apify resource)
        (catch Exception e (println e "\nCould not finish on: " (:name resource)))))))
